44b861b094a47eee45badd80f01ae818c9ef5ca5
brookerodenhizer <brooketaylorrodenhizer@gmail.com>
2017-10-04 22:41:24 -0400

    Checkpoint 12

--- scripts/album.js
+++ scripts/album.js
@@ -54,7 +54,7 @@ var albumPetty = {
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
        '<tr class="album-view-song-item">'
-     + '  <td class="song-item-number">' + songNumber + '</td>'
+     + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
      + '  <td class="song-item-title">' + songName + '</td>'
      + '  <td class="song-item-duration">' + songLength + '</td>'
      + '</tr>'
@@ -70,7 +70,7 @@ var albumImage = document.getElementsByClassName('album-cover-art')[0];
 var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
 var setCurrentAlbum = function(album) {
-    albumTitle.firstChild.nodeValue = album.name;
+    albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
     albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
     albumImage.setAttribute('src', album.albumArtUrl);
@@ -84,15 +84,32 @@ var setCurrentAlbum = function(album) {
     }
 };
 
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+ var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
 
+    songListContainer.addEventListener('mouseover', function(event) {
+    // #1
+    if (event.target.parentElement.className === 'album-view-song-item') {
+             event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+         }
+});
+
+  for (var i = 0; i < songRows.length; i++) {
+      songRows[i].addEventListener('mouseleave', function(event) {
+        this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+      });
+}
     var albums = [albumPicasso, albumMarconi, albumPetty];
     var index = 1;
-    albumImage.addEventListener("click", function(event){
+      albumImage.addEventListener("click", function(event){
         setCurrentAlbum(albums[index]);
         index++;
-        if (index == album.length) {
+        if (index == albums.length) {
           index = 0;
         }
     });
