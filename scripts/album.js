9c0f216b5d2ec5c9b8fba4f35dabf26c8cd22c94
brookerodenhizer <brooketaylorrodenhizer@gmail.com>
2017-11-18 15:08:24 -0500

    checkpoint 19

--- scripts/album.js
+++ scripts/album.js
@@ -1,55 +1,4 @@
-// Example Album
-var albumPicasso = {
-    title: 'The Colors',
-    artist: 'Pablo Picasso',
-    label: 'Cubism',
-    year: '1881',
-    albumArtUrl: 'assets/images/album_covers/01.png',
-    songs: [
-        { title: 'Blue', duration: '4:26' },
-        { title: 'Green', duration: '3:14' },
-        { title: 'Red', duration: '5:01' },
-        { title: 'Pink', duration: '3:21'},
-        { title: 'Magenta', duration: '2:15'}
-    ]
-};
-// Another Example Album
-var albumMarconi = {
-    title: 'The Telephone',
-    artist: 'Guglielmo Marconi',
-    label: 'EM',
-    year: '1909',
-    albumArtUrl: 'assets/images/album_covers/20.png',
-    songs: [
-        { title: 'Hello, Operator?', duration: '1:01' },
-        { title: 'Ring, ring, ring', duration: '5:01' },
-        { title: 'Fits in your pocket', duration: '3:21'},
-        { title: 'Can you hear me now?', duration: '3:14' },
-        { title: 'Wrong phone number', duration: '2:15'}
-    ]
-};
-//checkpoint 11 submission
-var albumPetty = {
-    title:  'Full Moon Fever',
-    artist: 'Tom Petty and the Heartbreakers',
-    label:  'MCA Records',
-    year:   '1989',
-    albumArtUrl:  'assets/images/album_covers/Tom_Petty_Album_Cover.jpg',
-    songs:  [
-        { title: 'Free Fallin', duration: '4:16'},
-        { title: 'I Wont Back Down', duration: '2:57'},
-        { title: 'Love is a Long Road', duration: '4:08'},
-        { title: 'A Face in the Crowd', duration: '3:59'},
-        { title: 'Runnin Down a Dream', duration: '4:52'},
-        { title: 'Fell a Whole Lot Better', duration: '2:50'},
-        { title: 'Yer So Bad', duration: '3:06'},
-        { title: 'Depending on You', duration: '2:49'},
-        { title: 'The Apartment Song', duration: '2:33'},
-        { title: 'Alright for Now', duration: '2:01'},
-        { title: 'A Mind With a Heart of Its Own', duration: '3:31'},
-        { title: 'Zombie Zoo', duration: '2:59'}
-    ]
-};
+
 
 var createSongRow = function(songNumber, songName, songLength) {
     var template =
@@ -64,21 +13,24 @@ var createSongRow = function(songNumber, songName, songLength) {
 
 
      var clickHandler = function() {
-     	var songNumber = $(this).attr('data-song-number');
+     	var songNumber = parseInt($(this).attr('data-song-number'));
 
-     	if (currentlyPlayingSong !== null) {
+     	if (currentlyPlayingSongNumber !== null) {
      		// Revert to song number for currently playing song because user started playing new song.
-     		var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-     		currentlyPlayingCell.html(currentlyPlayingSong);
+     		var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+     		currentlyPlayingCell.html(currentlyPlayingSongNumber);
      	}
-     	if (currentlyPlayingSong !== songNumber) {
+     	if (currentlyPlayingSongNumber !== songNumber) {
      		// Switch from Play -> Pause button to indicate new song is playing.
      		$(this).html(pauseButtonTemplate);
-     		currentlyPlayingSong = songNumber;
-     	} else if (currentlyPlayingSong === songNumber) {
+     		currentlyPlayingSongNumber = songNumber;
+        currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+     	} else if (currentlyPlayingSongNumber === songNumber) {
      		// Switch from Pause -> Play button to pause currently playing song.
      		$(this).html(playButtonTemplate);
-     		currentlyPlayingSong = null;
+        $('.main-controls .play-pause').html(playerBarPlayButton);
+     		currentlyPlayingSongNumber = null;
+        currentSongFromAlbum = null;
      	}
      };
 
@@ -86,7 +38,7 @@ var createSongRow = function(songNumber, songName, songLength) {
          var songNumberCell = $(this).find('.song-item-number');
          var songNumber = songNumberCell.attr('data-song-number');
 
-         if (songNumber !== currentlyPlayingSong) {
+         if (songNumber !== currentlyPlayingSongNumber) {
              songNumberCell.html(playButtonTemplate);
          }
      };
@@ -95,9 +47,10 @@ var createSongRow = function(songNumber, songName, songLength) {
          var songNumberCell = $(this).find('.song-item-number');
          var songNumber = songNumberCell.attr('data-song-number');
 
-         if (songNumber !== currentlyPlayingSong) {
+         if (songNumber !== currentlyPlayingSongNumber) {
              songNumberCell.html(songNumber);
          }
+         console.log("songNumber type is " + typeof songNumber + "\n and currentlyPlayingSongNumber type is " + typeof currentlyPlayingSongNumber);
      };
 
      // #1
@@ -111,6 +64,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 var $albumImage = $('.album-cover-art');
 
 var setCurrentAlbum = function(album) {
+  currentAlbum = album;
   var $albumTitle = $('.album-view-title');
   var $albumArtist = $('.album-view-artist');
   var $albumReleaseInfo = $('.album-view-release-info');
@@ -130,16 +84,76 @@ var setCurrentAlbum = function(album) {
     }
 };
 
+var trackIndex = function(album, song) {
+  return album.songs.indexOf(song);
+}
+
+var nextSong = function() {
+    var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+
+    currentSongIndex++;
+
+    if (currentSongIndex >= currentAlbum.songs.length) {
+        currentSongIndex = 0;
+    }
+
+  var lastSongNumber = currentlyPlayingSongNumber;
+  currentlyPlayingSongNumber = currentSongIndex + 1;
+  currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+
+  var previousSong = function() {
+      var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
+      // Note that we're _decrementing_ the index here
+      currentSongIndex--;
+
+      if (currentSongIndex < 0) {
+          currentSongIndex = currentAlbum.songs.length - 1;
+      }
+
+      var lastSongNumber = currentlyPlayingSongNumber;
+      currentlyPlayingSongNumber = currentSongIndex + 1;
+      currentSongFromAlbum = currentAlbum.songs[currentSongIndex];
+}
+
+var updatePlayerSongBar = function(){
+  $('.currently-playing .song-name').text(currentSongFromAlbum.title);
+  $('.currently-playing .artist-name').text(currentAlbum.artist);
+  $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.title + " - " + currentAlbum.artist);
+  $('.main-controls .play-pause').html(playerBarPauseButton);
+
+  var $previousSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+  var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+
+  $previousSongNumberCell.html(pauseButtonTemplate);
+  $lastSongNumberCell.html(lastSongNumber);
+
+  var $nextSongNumberCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
+  var $lastSongNumberCell = $('.song-item-number[data-song-number="' + lastSongNumber + '"]');
+
+  $nextSongNumberCell.html(pauseButtonTemplate);
+  $lastSongNumberCell.html(lastSongNumber);
+};
+
 
  var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
  var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
+ var playerBarPlayButton = '<span class="ion-play"></span>';
+ var playerBarPauseButtpn = '<span class="ion-pause"></span>';
 // Store state of playing songs
-var currentlyPlayingSong = null;
+
+var currentAlbum = null;
+var currentlyPlayingSongNumber = null;
+var currentSongFromAlbum = null;
+
+var $previousButton = $('.main-controls .previous');
+var $nextButton = $('.main-controls .next');
 
  $(document).ready(function() {
     setCurrentAlbum(albumPicasso);
+    $previousButton.click(previousSong);
+    $nextButton.click(nextSong);
   });
     var albums = [albumPicasso, albumMarconi, albumPetty];
     var index = 1;
---
+++ scripts/fixture.js
@@ -0,0 +1,52 @@
+// Example Album
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'}
+    ]
+};
+// Another Example Album
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+//checkpoint 11 submission
+var albumPetty = {
+    title:  'Full Moon Fever',
+    artist: 'Tom Petty and the Heartbreakers',
+    label:  'MCA Records',
+    year:   '1989',
+    albumArtUrl:  'assets/images/album_covers/Tom_Petty_Album_Cover.jpg',
+    songs:  [
+        { title: 'Free Fallin', duration: '4:16'},
+        { title: 'I Wont Back Down', duration: '2:57'},
+        { title: 'Love is a Long Road', duration: '4:08'},
+        { title: 'A Face in the Crowd', duration: '3:59'},
+        { title: 'Runnin Down a Dream', duration: '4:52'},
+        { title: 'Fell a Whole Lot Better', duration: '2:50'},
+        { title: 'Yer So Bad', duration: '3:06'},
+        { title: 'Depending on You', duration: '2:49'},
+        { title: 'The Apartment Song', duration: '2:33'},
+        { title: 'Alright for Now', duration: '2:01'},
+        { title: 'A Mind With a Heart of Its Own', duration: '3:31'},
+        { title: 'Zombie Zoo', duration: '2:59'}
+    ]
+};
