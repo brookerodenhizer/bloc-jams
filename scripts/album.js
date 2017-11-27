83e7fd3480793af8138ea3890907736c964d9a15
brookerodenhizer <brooketaylorrodenhizer@gmail.com>
2017-11-27 13:33:47 -0500

    checkpoint 19 update

--- scripts/album.js
+++ scripts/album.js
@@ -38,20 +38,15 @@ var createSongRow = function(songNumber, songName, songLength) {
      var clickHandler = function() {
      	var songNumber = parseInt($(this).attr('data-song-number'));
 
-      if (currentlyPlayingSongNumber !== null) {
-          // Revert to song number for currently playing song because user started playing new song.
-          var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
-
-          currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
-          currentlyPlayingCell.html(currentlyPlayingSongNumber);
-      }
-      if (currentlyPlayingSongNumber !== songNumber) {
-          // Switch from Play -> Pause button to indicate new song is playing.
-          setSong(songNumber);
-          currentSoundFile.play();
-          $(this).html(pauseButtonTemplate);
-          currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
-          updatePlayerBarSong();
+     	if (currentlyPlayingSongNumber !== null) {
+     		// Revert to song number for currently playing song because user started playing new song.
+     		var currentlyPlayingCell = getSongNumberCell(currentlyPlayingSongNumber);
+     		currentlyPlayingCell.html(currentlyPlayingSongNumber);
+     	}
+     	if (currentlyPlayingSongNumber !== songNumber) {
+     		// Switch from Play -> Pause button to indicate new song is playing.
+        setSong(songNumber);
+        $(this).html(pauseButtonTemplate);
      	} else if (currentlyPlayingSongNumber === songNumber) {
            if (currentSoundFile.isPaused()) {
                $(this).html(pauseButtonTemplate);
